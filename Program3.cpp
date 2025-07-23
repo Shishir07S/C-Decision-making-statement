97f379407ad4116894bc593709066233e0cbffa5
Shishir07S <swaroopshishir0112@gmail.com>
2025-07-24 02:58:41 +0530

    Update Program3.cpp

--- Program3.cpp
+++ Program3.cpp
@@ -1,23 +1,28 @@
+
 #include<iostream>
 using namespace std;
 int main()
 {
     int a,b,c;
-    cout<<"enter the number 1: "<<endl;
+    cout<<"enter the num 1: "<<endl;
     cin>>a;
-    cout<<"enter the number 2: "<<endl;
+    cout<<"enter the num 2: "<<endl;
     cin>>b;
-    cout<<"enter the number 3:"<<endl;
+    cout<<"enter the num 3:"<<endl;
     cin>>c;
-    if(a>b) and (a>c){
-      
-      cout<<a<<" is the greatest number."<<endl;
-    }else if (b>a) and (b>c){
-      
-      cout<<b<<" is the greatest number."<<endl;
-    
-    }else{
-      count<<c<<"is the greatest number."<<endl;
+    if(a>b)
+    {
+        if(a>c)
+        cout<<a<<" is the greatest number."<<endl;
+        else
+         cout<<c<<" is the greatest number."<<endl;
+    }
+    else
+    {
+        if(b>c)
+         cout<<a<<" is the greatest number."<<endl;
+        else
+         cout<<c<<" is the greatest number."<<endl;
     }
 }
-        
+    
